5670d571e200c4ea28a58edd131835f138c6cd85
Ali Nategh <alinategh005@gmail.com>
2023-02-16 21:04:03 +0330

    Create: header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import TrollFace from '../../public/TrollFace.png'
+
+export default function Header() {
+    return(
+        <header className='header'>
+            <img className='header--img' src={TrollFace} alt="Troll Face" />
+            <h2 className='header--title'>Meme Generator</h2>
+            <h4 className='header--project'>React Course - Project 3</h4>
+        </header>
+    )
+}
