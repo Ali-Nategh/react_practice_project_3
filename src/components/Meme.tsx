448f8e2ceefd6d6386f40d939c8bd11b3d6f65c8
Ali Nategh <alinategh005@gmail.com>
2023-02-17 15:44:32 +0330

    fix: a btn bug and img style

--- src/components/Meme.tsx
+++ src/components/Meme.tsx
@@ -61,8 +61,8 @@ export default function Meme() {
             </div>
             <div className="meme">
                 {meme.randomImage !== "" && <img src={meme.randomImage} className="meme--image" />}
-                <h2 className="meme--text top">{meme.topText}</h2>
-                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+                {meme.randomImage !== "" && <h2 className="meme--text top">{meme.topText}</h2>}
+                {meme.randomImage !== "" && <h2 className="meme--text bottom">{meme.bottomText}</h2>}
             </div>
         </main>
     )
